1928a7bd4950a9ee260fe67bf511aa4669a3b9be
toki-04 <toki.cc.04@gmail.com>
2024-05-06 09:43:36 +0800

    home page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("home.urls")),
     path("todo/", include("todo.urls")),
     path("qr/", include("qr_code_generator.urls")),
 ]
